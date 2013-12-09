60945329b6064fe0b923a30d23be5223e8cbebb3
franszo <franszo.design@gmail.com>
2013-12-09 11:17:10 +0000

    styles + equal height to bottom form 's radios
    Signed-off-by: franszo <franszo.design@gmail.com>

--- js/main.js
+++ js/main.js
@@ -22,10 +22,41 @@
 		            span.css('width',newW);
 		        });
 		    }
-		},	
-		
+		},
+
 		resize: function(){
 		}
+	}
+
+	equalheight = function(container){
+
+	var currentTallest = 0,
+	     currentRowStart = 0,
+	     rowDivs = new Array(),
+	     $el,
+	     topPosition = 0;
+	 $(container).each(function() {
+
+	   $el = $(this);
+	   $($el).height('auto')
+	   topPostion = $el.position().top;
+
+	   if (currentRowStart != topPostion) {
+	     for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+	       rowDivs[currentDiv].height(currentTallest);
+	     }
+	     rowDivs.length = 0; // empty the array
+	     currentRowStart = topPostion;
+	     currentTallest = $el.height();
+	     rowDivs.push($el);
+	   } else {
+	     rowDivs.push($el);
+	     currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+	  }
+	   for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+	     rowDivs[currentDiv].height(currentTallest);
+	   }
+	 });
 	}
 
 	$(function(){
@@ -34,7 +65,12 @@
 
 	$(window).load(function(){
 		main.loaded();
+	  	equalheight('.gfield_radio li');
 	});
+
+	$(window).resize(function(){
+	  equalheight('.gfield_radio li');
+	});	
 
 	$(window).resize(function(){
 	});
